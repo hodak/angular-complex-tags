a158f477f3c3724b334a6168f792220ae42d4bf7
hodak <kaczor555@gmail.com>
2017-02-18 21:08:31 +0100

    Add tagger component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TaggerComponent } from './tagger/tagger.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaggerComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/tagger/tagger.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tagger',
+  templateUrl: './tagger.component.html',
+  styleUrls: ['./tagger.component.css'],
+})
+export class TaggerComponent {
+  title = 'tagger works!';
+}
