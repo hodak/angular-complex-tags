1a42b276e1a8b22fea67142c868f01009e65df9d
hodak <kaczor555@gmail.com>
2017-02-19 09:31:01 +0100

    Add comma as default delimiter
    * This changes our approach to handling input, because comma is first
      delimiter we tried that is actually printable character - on key up
      is triggered with old input value, thanks to which we can call
      event.preventDefault() so it's never added

--- src/app/tagger/tagger.component.ts
+++ src/app/tagger/tagger.component.ts
@@ -8,15 +8,16 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class TaggerComponent {
   @Input() tags = [];
   @Input() singleTagTemplate;
-  @Input() delimiterKeyCodes = [32, 13]; // [space, enter]
+  @Input() delimiterKeyCodes = [32, 13, 188]; // [space, enter, comma]
   @Input() tagValidator = (_) => null;
   @Output() newTag = new EventEmitter();
   @Output() deleteTag = new EventEmitter();
 
   error;
 
-  onKeyUp(event, input) {
+  onKeyDown(event, input) {
     if(this.delimiterKeyCodes.indexOf(event.keyCode) === -1) { return }
+    event.preventDefault();
 
     input.value = input.value.trim();
     if(input.value.length === 0) { return }
